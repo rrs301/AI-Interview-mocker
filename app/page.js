69c2e665a70f4828f6ed15a5553bada8629e507a
Rahul Sanap <rahulsanap@Rahuls-MacBook-Air.local>
2024-06-05 22:23:23 -0400

    Ladning Page

--- app/page.js
+++ app/page.js
@@ -1,18 +1,21 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Header from "./dashboard/_components/Header";
+import { AtomIcon, Edit, Share2 } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
+      <Image src={'/grid.svg'} className="absolute z-[-10] w-full" 
+      width={1200} height={300} />
       <Header/>
-     <section className="bg-white dark:bg-gray-900">
+     <section className=" z-50">
     <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span className="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">Tubeguruji.com All new Apps</span> 
             <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </a>
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">AI Mock Interview Taker</h1>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Your Personal AI Interview Coach</h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Double your chances of landing that job offer
 
 with our AI-powered interview prep</p>
@@ -61,7 +64,69 @@ with our AI-powered interview prep</p>
         </div> 
     </div>
 </section>
+<section className="py-8 bg-white z-50 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+<h2 className="font-bold text-3xl">How it Works?</h2>
+<h2 className="text-md text-gray-500">Give mock interview in just 3 simplar easy step</h2>
+
+<div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <a
+        className="block rounded-xl border bg-white
+         border-gray-200 p-8 shadow-xl transition
+         hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+       <AtomIcon className='h-8 w-8'/>
+
+        <h2 className="mt-4 text-xl font-bold text-black">Write promot for your form</h2>
+
+        <p className="mt-1 text-sm text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border bg-white border-gray-200 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+      <Edit className='h-8 w-8'/>
+
+        <h2 className="mt-4 text-xl font-bold text-black">Edit Your form </h2>
+
+        <p className="mt-1 text-sm text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border bg-white border-gray-200 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+      <Share2 className='h-8 w-8' />
+
+        <h2 className="mt-4 text-xl font-bold text-black">Share & Start Accepting Responses</h2>
+
+        <p className="mt-1 text-sm text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
+
+    
     </div>
+
+    <div className="mt-12 text-center">
+      <a
+        href="/sign-in"
+        className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
+      >
+        Get Started Today
+      </a>
+    </div>
+    </section>
+  </div>
+ 
 
   );
 }
