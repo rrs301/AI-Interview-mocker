35ec99eda6eff141e5546429cf860c15c2cbbfab
Rahul Sanap <rahulsanap@Rahuls-MacBook-Air.local>
2024-06-06 23:02:18 -0400

    Update

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -40,7 +40,7 @@ function Feedback({params}) {
        <h2 className='text-3xl font-bold text-green-500'>Congratulation!</h2>
         <h2 className='font-bold text-2xl'>Here is your interview feedback</h2>
        
-        <h2 className='text-primary text-lg my-3'>Your overall interview rating: <strong>7/10</strong></h2>
+        {/* <h2 className='text-primary text-lg my-3'>Your overall interview rating: <strong>7/10</strong></h2> */}
    
         <h2 className='text-sm text-gray-500'>Find below interview question with correct answer, Your answer and feedback for improvement</h2>
         {feedbackList&&feedbackList.map((item,index)=>(
